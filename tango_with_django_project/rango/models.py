93dee4c10f9de653948576f2d8e8627013dfd69b
Rob-Aws <RMac@Roberts-MacBook-Air.local>
2017-02-03 18:58:58 +0000

    Chapter 9 complete

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -28,3 +29,18 @@ class Page(models.Model):
 
     def __unicode__(self):
         return str(self.title)
+
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+
+    # The additional attributes we wish to include.
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    # Remember if you use Python 2.7.x, define __unicode__ too!
+    def __unicode__(self):
+        return str(self.user.username)
+
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -133,3 +133,4 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [STATIC_DIR, ]
 
 STATIC_URL = '/static/'
+LOGIN_URL = '/rango/login/'
